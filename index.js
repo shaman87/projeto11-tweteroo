07dcad8ce6492c4377da5143020b7d1175146dd4
Felipe Albuquerque <rockalbuquerque@gmail.com>
2022-08-27 13:23:54 -0300

    Postar tweet

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import cors from "cors";
 
 const app = express();
@@ -12,7 +12,14 @@ app.post("/sign-up", (request, response) => {
     const { username, avatar } = request.body;
 
     usersList.push({ username, avatar });
-    response.send(usersList);
+    response.send("OK");
+});
+
+app.post("/tweets", (request, response) => {
+    const { username, tweet } = request.body;
+
+    tweetsList.push({ username, tweet });
+    response.send("OK");
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
