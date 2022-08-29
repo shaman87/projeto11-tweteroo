baf9b93fbfa4eb9e6f359c1f266540417d87485f
Felipe Albuquerque <rockalbuquerque@gmail.com>
2022-08-29 02:31:19 -0300

    Retorna os 10 últimos tweets publicados

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { request, response } from "express";
+import express from "express";
 import cors from "cors";
 
 const app = express();
@@ -12,14 +12,35 @@ app.post("/sign-up", (request, response) => {
     const { username, avatar } = request.body;
 
     usersList.push({ username, avatar });
-    response.send("OK");
+    response.send(usersList);
 });
 
 app.post("/tweets", (request, response) => {
     const { username, tweet } = request.body;
+    
+    if(usersList.length > 0) {
+        const avatar = usersList.find(value => value.username === username).avatar;
+        tweetsList.push({ username, avatar, tweet });
+        response.send("OK");
+    }
 
-    tweetsList.push({ username, tweet });
-    response.send("OK");
+    response.send("Error");
+});
+
+app.get("/tweets", (request, response) => {
+    const showTweets = [];
+
+    if(tweetsList.length >= 10) {
+        for(let i = tweetsList.length - 1; i >= tweetsList.length - 10; i--) {
+            showTweets.push(tweetsList[i]);
+        }
+    } else {
+        for(let i = tweetsList.length - 1; i >= 0; i--) {
+            showTweets.push(tweetsList[i]);
+        }
+    }
+
+    response.send(showTweets);
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
